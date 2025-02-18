ac928eeb0e57a471ffb03b74bf77aa4d9dfaab16
NeerajVishwasrao <neerajvishwasrao@gmail.com>
2025-02-18 11:39:45 +0530

    new project for shwoing teams , scores , players for tournament and component added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { MatchDashboardComponent } from './match-dashboard/match-dashboard.component';
+import { ControlPanelComponent } from './control-panel/control-panel.component';
+import { PlayersComponent } from './players/players.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: MatchDashboardComponent },
+    { path: "controlPanel", component: ControlPanelComponent },
+    { path: "players", component: PlayersComponent },
+    {path:"adminLogin",component:AdminLoginComponent}
+];
---
+++ src/app/match-dashboard/match-dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-match-dashboard',
+  imports: [],
+  templateUrl: './match-dashboard.component.html',
+  styleUrl: './match-dashboard.component.css'
+})
+export class MatchDashboardComponent {
+
+}
