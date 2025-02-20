e59d39bc86e5ba261f58c6730ad0a5fab392c5a6
Aditya <bhoraditya7635@gmail.com>
2025-02-20 17:44:07 +0530

    player component first edit

--- src/app/players/players.component.ts
+++ src/app/players/players.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-players',
@@ -7,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrl: './players.component.css'
 })
 export class PlayersComponent {
+  router = inject(Router);
+  http = inject(HttpClient);
 
+
+  players:any=[
+    {srno: 1, name:"Mark Otto", class:"12th div-A"},
+    {srno: 2, name:"Mark Otto", class:"12th div-A"},
+    {srno: 3, name:"Mark Otto", class:"12th div-A"},
+    {srno: 4, name:"Mark Otto", class:"12th div-A"},
+    {srno: 5, name:"Mark Otto", class:"12th div-A"},
+    {srno: 6, name:"Mark Otto", class:"12th div-A"},
+    {srno: 7, name:"Mark Otto", class:"12th div-A"},
+    {srno: 8, name:"Mark Otto", class:"12th div-A"},
+    {srno: 9, name:"Mark Otto", class:"12th div-A"},
+  ]
 }
